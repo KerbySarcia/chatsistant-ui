98dbc556b2c57c592698f52790ca7f72934eabe2
Kerby Sarcia <kosarcia@rpinnotech.com>
2023-11-13 20:47:43 +0800

    update dashboard

--- src/pages/Staff/pages/home/index.jsx
+++ src/pages/Staff/pages/home/index.jsx
@@ -4,18 +4,23 @@ import useKnowledege from "../../../../services/useKnowledge";
 import LoadingSpinner from "../../../../components/LoadingSpinner";
 import useInquiryService from "../../../../services/useInquiryService";
 import useSession from "../../../../hooks/useSession";
+import useUserService from "../../../../services/useUserService";
 
 const Home = () => {
   const [knowledges, setKnowledges] = useState(null);
   const [numberOfPending, setNumberOfPending] = useState(null);
+  const [users, setUsers] = useState(null);
   const knowldegeService = useKnowledege();
   const inquiryService = useInquiryService();
+  const userService = useUserService();
   const { session } = useSession();
 
   useEffect(() => {
     (async () => {
       const responseKnowledge = await knowldegeService.getKnowledges();
       const responseInquiry = await inquiryService.getAll();
+      const responseUser = await userService.getUsers();
+      setUsers(responseUser.filter(user => user.role === "USER").length);
       setKnowledges(responseKnowledge.items);
       setNumberOfPending(
         responseInquiry.items.filter(inquiry => inquiry.status === "PENDING")
@@ -24,7 +29,7 @@ const Home = () => {
     })();
   }, []);
 
-  if (!knowledges && !numberOfPending)
+  if (!knowledges && !numberOfPending && !users)
     return (
       <div className="flex h-full w-full items-center justify-center">
         <LoadingSpinner />
@@ -62,13 +67,22 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="flex h-full w-full items-center justify-center gap-5 rounded-lg bg-black/30 p-5 text-white">
-        <Icon icon={"solar:chart-outline"} className="text-9xl" />
-        <div className="flex flex-col ">
-          <span className="font-productSansBlack text-9xl">1137</span>
-          <span className="text-2xl">
-            Received inquiries by the DHVChat AI Chat Assistant
-          </span>
+      <div className="flex h-full gap-5">
+        <div className="flex h-full w-full items-center justify-center gap-5 rounded-lg bg-black/30 p-5 text-white">
+          <Icon icon={"solar:chart-outline"} className="text-[200px]" />
+          <div className="flex flex-col ">
+            <span className="font-productSansBlack text-9xl">1137</span>
+            <span className="text-2xl">
+              Received inquiries by the DHVChat AI Chat Assistant
+            </span>
+          </div>
+        </div>
+        <div className="flex h-full w-full items-center justify-center gap-3 rounded-lg bg-black/30 p-5 text-white">
+          <Icon icon={"tabler:user-filled"} className="text-[200px]" />
+          <div className="flex flex-col ">
+            <span className="font-productSansBlack text-8xl">{users}</span>
+            <span className="text-2xl">Registered Users</span>
+          </div>
         </div>
       </div>
     </div>
